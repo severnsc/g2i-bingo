687fb629b31cb1576d7e3f393c69b2324f04dcff
Nathan Thomas <nwthomas@me.com>
2018-12-05 11:23:02 -0800

    Create IIFE and add str to joshPhrases

--- scripts/index.js
+++ scripts/index.js
@@ -44,10 +44,11 @@ let joshPhrases = [
   { text: '"Pro tip!"' },
   { text: '"In the wild"' },
   { text: '"Banana"' },
-  { text: 'Draw tool is used' },
+  { text: "Draw tool is used" },
   { text: '"Under the hood"' },
-  { text: '(Dad cough)' },
-  { text: '"Mind yer loggers!"' }
+  { text: "(Dad cough)" },
+  { text: '"Mind yer loggers!"' },
+  { text: '"Whoa."' }
 ];
 function createRandomArray() {
   const day = new Date().getDay();
@@ -132,7 +133,7 @@ function dustinBingo() {
 function otherBingo() {
   alert("That feature will be added once we get more instructors.");
 }
-function listenNavBtns() {
+(function listenNavBtns() {
   buttons.forEach(input => {
     input.addEventListener("click", () => {
       if (input.value === "Josh") {
@@ -146,9 +147,7 @@ function listenNavBtns() {
       }
     });
   });
-}
-
-
+})();
 function gotABingo(sq1, sq2, sq3, sq4, sq5) {
   sq1.style.backgroundColor = "yellow";
   sq2.style.backgroundColor = "yellow";
@@ -157,112 +156,104 @@ function gotABingo(sq1, sq2, sq3, sq4, sq5) {
   sq5.style.backgroundColor = "yellow";
   alert("BINGO! Nice one.");
 }
-
-
 function checkLines(sq1, sq2, sq3, sq4, sq5) {
-  if (sq1.style.backgroundColor == shadedColor &&
-      sq2.style.backgroundColor == shadedColor &&
-      sq3.value == "Free Space" &&
-      sq4.style.backgroundColor == shadedColor &&
-      sq5.style.backgroundColor == shadedColor) {
-          gotABingo(sq1, sq2, sq3, sq4, sq5);
-          return;
-  }
-  else if (sq1.style.backgroundColor == shadedColor &&
-          sq2.style.backgroundColor == shadedColor &&
-          sq3.style.backgroundColor == shadedColor && 
-          sq4.style.backgroundColor == shadedColor &&
-          sq5.style.backgroundColor == shadedColor ) {
-              gotABingo(sq1, sq2, sq3, sq4, sq5);
-              return;
+  if (
+    sq1.style.backgroundColor == shadedColor &&
+    sq2.style.backgroundColor == shadedColor &&
+    sq3.value == "Free Space" &&
+    sq4.style.backgroundColor == shadedColor &&
+    sq5.style.backgroundColor == shadedColor
+  ) {
+    gotABingo(sq1, sq2, sq3, sq4, sq5);
+    return;
+  } else if (
+    sq1.style.backgroundColor == shadedColor &&
+    sq2.style.backgroundColor == shadedColor &&
+    sq3.style.backgroundColor == shadedColor &&
+    sq4.style.backgroundColor == shadedColor &&
+    sq5.style.backgroundColor == shadedColor
+  ) {
+    gotABingo(sq1, sq2, sq3, sq4, sq5);
+    return;
   }
 }
-
-
 function checkVerticalBingo() {
   for (var i = 0; i < 5; i++) {
-      var sq1 = document.getElementById('box' + i);
-      var sq2 = document.getElementById('box' + (i + 5));
-      var sq3 = document.getElementById('box' + (i + 10));
-      var sq4 = document.getElementById('box' + (i + 15));
-      var sq5 = document.getElementById('box' + (i + 20));
-      checkLines(sq1, sq2, sq3, sq4, sq5);
+    var sq1 = document.getElementById("box" + i);
+    var sq2 = document.getElementById("box" + (i + 5));
+    var sq3 = document.getElementById("box" + (i + 10));
+    var sq4 = document.getElementById("box" + (i + 15));
+    var sq5 = document.getElementById("box" + (i + 20));
+    checkLines(sq1, sq2, sq3, sq4, sq5);
   }
 }
-
 function checkHorizontalBingo() {
   j = 0;
   for (var i = 0; i < 5; i++) {
-      switch(i) {
-          case 0: 
-              var sq1 = document.getElementById('box' + i);
-              var sq2 = document.getElementById('box' + (i + 1));
-              var sq3 = document.getElementById('box' + (i + 2));
-              var sq4 = document.getElementById('box' + (i + 3));
-              var sq5 = document.getElementById('box' + (i + 4));
-              break;
-          case 1: 
-              var sq1 = document.getElementById('box' + (i + 4));
-              var sq2 = document.getElementById('box' + (i + 5));
-              var sq3 = document.getElementById('box' + (i + 6));
-              var sq4 = document.getElementById('box' + (i + 7));
-              var sq5 = document.getElementById('box' + (i + 8));
-              break;
-          case 2: 
-              var sq1 = document.getElementById('box' + (i + 8));
-              var sq2 = document.getElementById('box' + (i + 9));
-              var sq3 = document.getElementById('box' + (i + 10));
-              var sq4 = document.getElementById('box' + (i + 11));
-              var sq5 = document.getElementById('box' + (i + 12));
-              break;
-          case 3: 
-              var sq1 = document.getElementById('box' + (i + 12));
-              var sq2 = document.getElementById('box' + (i + 13));
-              var sq3 = document.getElementById('box' + (i + 14));
-              var sq4 = document.getElementById('box' + (i + 15));
-              var sq5 = document.getElementById('box' + (i + 16));
-              break;
-          case 4: 
-              var sq1 = document.getElementById('box' + (i + 16));
-              var sq2 = document.getElementById('box' + (i + 17));
-              var sq3 = document.getElementById('box' + (i + 18));
-              var sq4 = document.getElementById('box' + (i + 19));
-              var sq5 = document.getElementById('box' + (i + 20));
-              break;
-      }
-      checkLines(sq1, sq2, sq3, sq4, sq5);
+    switch (i) {
+      case 0:
+        var sq1 = document.getElementById("box" + i);
+        var sq2 = document.getElementById("box" + (i + 1));
+        var sq3 = document.getElementById("box" + (i + 2));
+        var sq4 = document.getElementById("box" + (i + 3));
+        var sq5 = document.getElementById("box" + (i + 4));
+        break;
+      case 1:
+        var sq1 = document.getElementById("box" + (i + 4));
+        var sq2 = document.getElementById("box" + (i + 5));
+        var sq3 = document.getElementById("box" + (i + 6));
+        var sq4 = document.getElementById("box" + (i + 7));
+        var sq5 = document.getElementById("box" + (i + 8));
+        break;
+      case 2:
+        var sq1 = document.getElementById("box" + (i + 8));
+        var sq2 = document.getElementById("box" + (i + 9));
+        var sq3 = document.getElementById("box" + (i + 10));
+        var sq4 = document.getElementById("box" + (i + 11));
+        var sq5 = document.getElementById("box" + (i + 12));
+        break;
+      case 3:
+        var sq1 = document.getElementById("box" + (i + 12));
+        var sq2 = document.getElementById("box" + (i + 13));
+        var sq3 = document.getElementById("box" + (i + 14));
+        var sq4 = document.getElementById("box" + (i + 15));
+        var sq5 = document.getElementById("box" + (i + 16));
+        break;
+      case 4:
+        var sq1 = document.getElementById("box" + (i + 16));
+        var sq2 = document.getElementById("box" + (i + 17));
+        var sq3 = document.getElementById("box" + (i + 18));
+        var sq4 = document.getElementById("box" + (i + 19));
+        var sq5 = document.getElementById("box" + (i + 20));
+        break;
+    }
+    checkLines(sq1, sq2, sq3, sq4, sq5);
   }
 }
-
 function checkDiagonalBingo() {
   for (var i = 0; i < 2; i++) {
-      switch(i) {
-          case 0:
-              var sq1 = document.getElementById('box' + 0);
-              var sq2 = document.getElementById('box' + 6);
-              var sq3 = document.getElementById('box' + 12);
-              var sq4 = document.getElementById('box' + 18);
-              var sq5 = document.getElementById('box' + 24);
-              break;
-          case 1:
-              var sq1 = document.getElementById('box' + 4);
-              var sq2 = document.getElementById('box' + 8);
-              var sq3 = document.getElementById('box' + 12);
-              var sq4 = document.getElementById('box' + 16);
-              var sq5 = document.getElementById('box' + 20);
-              break;
-      }
-      checkLines(sq1, sq2, sq3, sq4, sq5);
+    switch (i) {
+      case 0:
+        var sq1 = document.getElementById("box" + 0);
+        var sq2 = document.getElementById("box" + 6);
+        var sq3 = document.getElementById("box" + 12);
+        var sq4 = document.getElementById("box" + 18);
+        var sq5 = document.getElementById("box" + 24);
+        break;
+      case 1:
+        var sq1 = document.getElementById("box" + 4);
+        var sq2 = document.getElementById("box" + 8);
+        var sq3 = document.getElementById("box" + 12);
+        var sq4 = document.getElementById("box" + 16);
+        var sq5 = document.getElementById("box" + 20);
+        break;
+    }
+    checkLines(sq1, sq2, sq3, sq4, sq5);
   }
 }
-
 function checkBingo() {
   checkVerticalBingo();
   checkHorizontalBingo();
   checkDiagonalBingo();
 }
-
-
-
-listenNavBtns(); // Adds even listeners to buttons
 joshBingo(); // Builds a bingo board for Josh as default - Can be changed when cohort moves on
